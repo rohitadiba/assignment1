698d92eba4424e09cb8a9447e221e01fad4a8c92
rohitadiba <rohitiqbal27@gmail.com>
2025-09-01 19:33:27 +0600

    my first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('heart')
+.addEventListener('click',function(){
+    const clickk=document.getElementById('heart')
+ const zeroo=parseInt(document.getElementById('zero').innerText)
+ const tatallove=clickk + zeroo
+ document.getElementById('zero').innerText=tatallove
+
+})
